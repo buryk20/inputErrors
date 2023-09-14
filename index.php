d642bbad802adc5fe124189b8312be3bb4361d7e
buryk20 <73736907+buryk20@users.noreply.github.com>
2023-09-14 12:07:43 +0300

    start

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'Вы ввели некорректный email' => '@You entered an incorrect email',
+    'Вы ввели некорректный номер телефона' => '@You entered an incorrect phone number',
+    'Поле ввода обязательное' => '@Entry field required',
+    'Поле выбора обязательное' => '@Selection field is required',
+    'Минимальная длина пароля' => '@Minimum password length',
+    'Пароль должен содержать минимум одну цифру' => '@The password must contain at least one number',
+    'Пароли не совпадают' => '@Password mismatch',
+    'Пользователь с такими данными не зарегистрирован, пройдите регистрацию' => '@The user with this data is not registered, please register',
+    'Неверный пароль' => '@Incorrect password',
+    'Максимальная длина поля' => '@Maximum field length',
+    'Неверный формат ИНН' => '@Invalid TIN format',
+    'Неверный формат ЕГРПОУ' => '@Invalid EDRPOU format'
+];
